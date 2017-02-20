a762c815660cdb9a71174858171f46fe34628404
Feroz Rauf <feroz.rauf@smartfinancellc.net>
2017-02-19 20:46:39 -0500

    first site

--- car_scrape.js
+++ car_scrape.js
@@ -5,3 +5,32 @@ var cheerio = require('cheerio');
 var app     = express();
 
 
+function grabInventoryPerSite()
+{
+	return function(error,response,body) {
+		try {
+			$ = cheerio(body);
+			var prices = $('span.value');
+			var details = ${'dd'};
+			console.log(prices);
+			for(var i=0;i<prices.length;i++)
+			{
+				console.log(prices[i]);
+			}
+			console.log(details.text());
+		} catch(e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+var num_cars = 200;
+
+var site_url = 'http://www.automaxxcalgary.com/used-inventory/index.htm';
+
+
+for(var i=0; i<200; i+=16;)
+{
+	request(site_url + '?start=' + i,grabInventoryPerSite())
+
+}
