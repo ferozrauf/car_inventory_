274f66249fddb16abc27aa78602269eb56d1f3de
Feroz Rauf <feroz.rauf@smartfinancellc.net>
2017-02-20 21:28:26 -0500

    first site done

--- car_scrape.js
+++ car_scrape.js
@@ -3,34 +3,60 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
-
+var sum = 0;
+var total_cars = new Array();
 
 function grabInventoryPerSite()
 {
 	return function(error,response,body) {
 		try {
-			$ = cheerio(body);
-			var prices = $('span.value');
-			var details = ${'dd'};
-			console.log(prices);
-			for(var i=0;i<prices.length;i++)
+			var $ = cheerio.load(body);
+			var cars = $('li.item');
+			var prices = $('span.internetPrice.final-price').find('span.value');
+			var details = $('dd');
+			var cars = new Array();
+			for(var i=0;i<16;i++)
 			{
-				console.log(prices[i]);
+				var car = new Object();
+				if(prices[i].children!=undefined)
+				{
+					var pre_price = prices[i].children[0].data.replace(/\D/g,'');
+					car.price = parseInt(pre_price);
+					//console.log(car);
+					cars.push(car);
+				}
 			}
-			console.log(details.text());
+			var i =0;
+			var k = 0;
+			for(var j=0;j<details.length;j++)
+			{
+				cars[i][k++] = details[j].children[0].data;
+				if(k==8)
+				{
+					i++;
+					k=0;
+				}
+			}
+			sum+=cars.length;
+			for(var k=0;k<cars.length;k++)
+				console.log(cars[k]);
+			console.log(sum);
 		} catch(e) {
-			e.printStackTrace();
+			console.log(e);
 		}
 	}
 }
 
-var num_cars = 200;
+var num_cars = 207;
 
 var site_url = 'http://www.automaxxcalgary.com/used-inventory/index.htm';
 
 
-for(var i=0; i<200; i+=16;)
+for(var i=0; i<207; i+=16)
 {
-	request(site_url + '?start=' + i,grabInventoryPerSite())
+	if(i<207)
+		request(site_url + '?start=' + i,grabInventoryPerSite());
+	console.log(site_url + '?start=' + i);
+
 
 }
